89b6129f622b245880631e37d78e6765fd43c658
rahul MALAWADKAR <49741611+relaxedboi@users.noreply.github.com>
2025-07-28 21:44:36 +0530

    Update Application.java

--- datahub-frontend/app/controllers/Application.java
+++ datahub-frontend/app/controllers/Application.java
@@ -3,7 +3,6 @@ package controllers;
 import static auth.AuthUtils.ACTOR;
 import static auth.AuthUtils.SESSION_COOKIE_GMS_TOKEN_NAME;
 
-import akka.util.ByteString;
 import auth.Authenticator;
 import com.datahub.authentication.AuthenticationConstants;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -24,6 +23,7 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
+import org.apache.pekko.util.ByteString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import play.Environment;
@@ -190,8 +190,9 @@ public class Application extends Controller {
   }
 
   private HttpRequest.BodyPublisher buildBodyPublisher(Http.Request request) {
-    if (request.body().asBytes() != null) {
-      return HttpRequest.BodyPublishers.ofByteArray(request.body().asBytes().toArray());
+    ByteString bytes = request.body().asBytes();
+    if (bytes != null) {
+      return HttpRequest.BodyPublishers.ofByteArray(bytes.toArray());
     } else if (request.body().asText() != null) {
       return HttpRequest.BodyPublishers.ofString(request.body().asText());
     }
