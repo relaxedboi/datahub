c7604c139a69f0ea8ec58e56ca38c117d2be1af5
rahul MALAWADKAR <49741611+relaxedboi@users.noreply.github.com>
2025-07-28 21:50:59 +0530

    Update CustomAkkaHttpServer.scala

--- datahub-frontend/app/server/CustomAkkaHttpServer.scala
+++ datahub-frontend/app/server/CustomAkkaHttpServer.scala
@@ -5,6 +5,9 @@ import play.core.server.PekkoHttpServer
 import play.core.server.ServerProvider
 import org.apache.pekko.http.scaladsl.settings.ParserSettings
 
+/** Custom Pekko HTTP server that allows us to overrides some Pekko server settings as the current Play / Pekko
+ *  versions we're using don't allow us to override these via conf files
+ */
 class CustomPekkoHttpServer(context: PekkoHttpServer.Context) extends PekkoHttpServer(context) {
 
   override protected def createParserSettings(): ParserSettings = {
@@ -20,6 +23,7 @@ class CustomPekkoHttpServer(context: PekkoHttpServer.Context) extends PekkoHttpS
   }
 }
 
+/** A factory that instantiates a CustomPekkoHttpServerProvider. */
 class CustomPekkoHttpServerProvider extends ServerProvider {
   def createServer(context: ServerProvider.Context) = {
     val serverContext = PekkoHttpServer.Context.fromServerProviderContext(context)
